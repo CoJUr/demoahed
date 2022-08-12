cfcb45fbc44c2e95fb1f041036890920f48182b0
“Cory <coryurton@gmail.com>
2022-08-11 23:47:48 -0500

    Initial commit

---
+++ src/main/java/com/example/demoahed/DemoahedApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoahed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoahedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoahedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoahed/DemoahedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoahed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoahedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
