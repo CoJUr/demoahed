b251ffb57975c590ab7f07f96c9631bae16bc8d4
“Cory <coryurton@gmail.com>
2022-08-12 19:12:41 -0500

    added properties to gitignore

--- src/main/java/com/example/demoahed/DemoahedApplication.java
+++ src/main/java/com/example/demoahed/DemoahedApplication.java
@@ -1,13 +1,19 @@
 package com.example.demoahed;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan({"controller", "service"})
 public class DemoahedApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoahedApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoahedApplication.class, args);
+    }
 
 }
--- src/main/java/service/PersonService.java
+++ src/main/java/service/PersonService.java
@@ -1,2 +1,39 @@
-package service;public class PersonService {
+package service;
+
+import model.Person;
+import org.springframework.stereotype.Service;
+import java.util.Hashtable;
+
+@Service
+public class PersonService {
+
+    Hashtable<String, Person> persons = new Hashtable<>();
+
+    public PersonService() {
+        Person p = new Person();
+        p.setId("1");
+        p.setAge(20);
+        p.setFirstName("John");
+        p.setLastName("Doe");
+        persons.put(p.getId(), p);
+
+        p = new Person();
+        p.setId("2");
+        p.setAge(30);
+        p.setFirstName("Jane");
+        p.setLastName("Doe");
+        persons.put("2", p);
+    }
+
+    public Person getPerson(String id) {
+        if (persons.containsKey(id))
+            return persons.get(id);
+        else
+            return null;
+    }
+
+    public Hashtable<String, Person> getAll() {
+        return persons;
+    }
+
 }
